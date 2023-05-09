e4c4554c8db89224e1847a99321899eaf7e21e7c
arkhanis <casanovac@gmail.com>
2023-05-08 22:54:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/* Ejercicio 1
+let nombreEj1 = prompt ("Ingrese su nombre:")
+let apellidoEj1 = prompt ("Ingrese su apellido:")
+console.log("Bienvenido " +nombreEj1+" "+ apellidoEj1)
+*/
+
+/* Ejercicio 2
+let diametroEj2 = prompt ("Ingrese el diametro del círculo:")
+const pi = 3.1416;
+let perimetro = 2*pi*(diametroEj2/2)
+let area = pi*(diametroEj2/2)*(diametroEj2/2)
+console.log("El perímetro es: "+ perimetro)
+console.log("El área es: "+ area)
+*/
+
+/* Ejercicio 3
+let num1Ej3 = prompt ("Ingrese el primer número:")
+let num2Ej3 = prompt ("Ingrese el segundo número:")
+let num3Ej3 = prompt ("Ingrese el tercer número:")
+let num4Ej3 = prompt ("Ingrese el cuarto número:")
+
+num1Ej3 = parseInt(num1Ej3)  // para convertirlo en int
+num2Ej3 = parseInt(num2Ej3)
+num3Ej3 = parseInt(num3Ej3)
+num4Ej3 = parseInt(num4Ej3)
+
+let sumEj3 = num1Ej3 + num2Ej3 + num3Ej3 + num4Ej3
+console.log("La suma es igual a: "+ sumEj3)
+var promedioEj3 = (num1Ej3 + num2Ej3 + num3Ej3 + num4Ej3)/4
+console.log("El promedio es: "+promedioEj3)
+*/
+
+/* Ejercicio 4
+let valorHora = prompt ("Ingrese el valor de la hora (en USD) del colaborador: ")
+let cantHoras = prompt ("Ingrese la cantidad de horas laborales en el mes: ")
+let sueldo = valorHora * cantHoras
+console.log("El sueldo mensual en USD del colaborador es : "+sueldo)
+*/
